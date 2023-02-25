ff287c8106c51192145e3342388489a468a26ac2
Mura <muramarch@gmail.com>
2023-02-25 16:25:30 +0600

    Завершенный Todo

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ from modules import Todo, TodoItem
 
 def main():
     my_todo = Todo()
-    go_todo = Todo()
-
-    my_task = TodoItem('The find method should take a word as an argument and output a list of TodoItem instances that contain this word as a tuple, which will have the format (index, instance).')
-    my_task2 = TodoItem('Prepare to eat')
-
-    my_todo.add_todo(my_task)
-    my_task2.check()
-    my_todo.add_todo(my_task2)
-
+ 
+    while True:
+        task = input("Add new task and press 'q' for break: ")
+ 
+        if task == 'q':
+            break
+ 
+        my_task = TodoItem(task)
+        my_todo.add_todo(my_task)
+ 
     my_todo.list_items()
+ 
+    print(my_todo.find('Eating'))
 
-    my_task2.uncheck()
-
-    my_todo.list_items()
-
-    print(my_todo.find('word'))
 
 if __name__ == '__main__':
     main()
